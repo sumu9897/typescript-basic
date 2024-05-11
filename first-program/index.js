f107244ac2d25de91a39ac37fe6c5ad214bebb9f
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-11 12:16:42 +0600

    First Program

---
+++ first-program/index.js
@@ -0,0 +1,5 @@
+var userName = "Mohammd Sumon";
+var age = 24;
+console.log(userName, age);
+//Run process
+//tsc index.ts -> index.js -> node index.js
---
+++ first-program/index.ts
@@ -0,0 +1,9 @@
+let userName = "Mohammd Sumon";
+let age = 24;
+console.log(userName,age);
+
+
+
+
+//Run process
+//tsc index.ts -> index.js -> node index.js
