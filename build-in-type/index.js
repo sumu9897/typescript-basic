9c8de9f275688fc5cdcf51a0b14b5be083ab612a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-11 20:26:19 +0600

    Build in Type

---
+++ build-in-type/index.js
@@ -0,0 +1,10 @@
+// build-in data type : number, string ,boolean, void ,undefined , null
+var userId;
+var firstName;
+var lastName;
+var isActivated;
+userId = 101;
+firstName = "Mohammad";
+lastName = " Sumon";
+isActivated = true;
+console.log("Your id: ".concat(userId, ", user name: ").concat(firstName, " ").concat(lastName, ",account activated: ").concat(isActivated, " "));
---
+++ build-in-type/index.ts
@@ -0,0 +1,14 @@
+// build-in data type : number, string ,boolean, void ,undefined , null
+
+
+let userId : number;
+let firstName : string;
+let lastName : string;
+let isActivated : boolean;
+
+userId = 101;
+firstName = "Mohammad";
+lastName = " Sumon";
+isActivated = true;
+
+console.log( `Your id: ${userId}, user name: ${firstName} ${lastName},account activated: ${isActivated} `)
