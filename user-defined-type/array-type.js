747daaf01dd4921906f8b3c0647dda5fdf64f79a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-11 22:12:12 +0600

    User Defined Type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,6 @@
+var userNames;
+// let userNames : Array<string>;
+userNames = ["sumon", "jibon", "maha"];
+console.log(userNames[0]);
+var multiplyTypes;
+multiplyTypes = [1, "name"];
--- user-defined-type/array-type.ts
+++ user-defined-type/array-type.ts
@@ -1,1 +1,11 @@
-let userName = ["sumon" , "jibon" , "maha"]
+let userNames : string[];
+// let userNames : Array<string>;
+
+userNames = ["sumon" , "jibon" , "maha"];
+
+
+console.log(userNames[0]);
+
+let multiplyTypes : (string | number)[];
+
+multiplyTypes = [1 , "name"]
