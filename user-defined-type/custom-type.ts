b5471ccb43572724be487b5805dbc71b6a2d83da
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:53:11 +0600

    custom type

--- user-defined-type/custom-type.ts
+++ user-defined-type/custom-type.ts
@@ -1,15 +1,17 @@
 let users : object[];
 users = [];
 
-let user1 : { userName : string; userId: number};
+type User = { userName : string; userId: number}
+
+let user1 : User;
 user1 = {userName: "mohammad", userId:101};
 users.push(user1);
 
-let user2 : { userName : string; userId: number};
+let user2 : User;
 user2 = {userName: "sumon", userId:102};
 users.push(user2);
 
-let user3 : { userName : string; userId: number};
+let user3 : User;
 user3 = {userName: "jibon", userId:103};
 users.push(user3);
 
