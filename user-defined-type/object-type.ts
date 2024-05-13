374e32b5a10e267b3cde1de28cbd85818c159b81
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:34:37 +0600

    object type

--- user-defined-type/object-type.ts
+++ user-defined-type/object-type.ts
@@ -3,6 +3,6 @@ let users: object[];
 
 
 
-let user1: {userName:string, userId :number}; 
+let user1: {userName:string, userId? :number}; 
 user1 = {userName: "mohammad", userId :101};
 console.log(user1);
