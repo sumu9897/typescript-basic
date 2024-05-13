0dac545b43c2449580935e7a4ec17771f6a9df8d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:51:07 +0600

    object type

--- user-defined-type/custom-type.js
+++ user-defined-type/custom-type.js
@@ -1,5 +1,12 @@
 var users;
 users = [];
 var user1;
-user1 = { userName: "anis", userId: 101 };
+user1 = { userName: "mohammad", userId: 101 };
 users.push(user1);
+var user2;
+user2 = { userName: "sumon", userId: 102 };
+users.push(user2);
+var user3;
+user3 = { userName: "jibon", userId: 103 };
+users.push(user3);
+console.log(users);
--- user-defined-type/custom-type.ts
+++ user-defined-type/custom-type.ts
@@ -2,5 +2,15 @@ let users : object[];
 users = [];
 
 let user1 : { userName : string; userId: number};
-user1 = {userName: "anis", userId:101};
+user1 = {userName: "mohammad", userId:101};
 users.push(user1);
+
+let user2 : { userName : string; userId: number};
+user2 = {userName: "sumon", userId:102};
+users.push(user2);
+
+let user3 : { userName : string; userId: number};
+user3 = {userName: "jibon", userId:103};
+users.push(user3);
+
+console.log(users);
