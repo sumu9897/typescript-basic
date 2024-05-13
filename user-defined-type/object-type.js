1a51e716612bb33958bd542f286d75077a09aff5
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:36:14 +0600

    object type

--- user-defined-type/object-type.js
+++ user-defined-type/object-type.js
@@ -1,4 +1,6 @@
 var users;
+users = [];
 var user1;
-user1 = { userName: "mohammad", userId: 101 };
-console.log(user1);
+// user1 = {userName: "mohammad", userId :101};
+users.push(user1);
+console.log(users);
--- user-defined-type/object-type.ts
+++ user-defined-type/object-type.ts
@@ -1,8 +1,10 @@
 let users: object[];
+users=[]
 
 
 
 
-let user1: {userName:string, userId? :number}; 
-user1 = {userName: "mohammad", userId :101};
-console.log(user1);
+let user1: {userName:string, userId :number}; 
+// user1 = {userName: "mohammad", userId :101};
+users.push(user1);
+console.log(users);
