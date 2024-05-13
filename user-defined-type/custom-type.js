619223312bfcdab1e16b08336948b0e543ec015b
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:48:24 +0600

    object type

---
+++ user-defined-type/custom-type.js
@@ -0,0 +1,5 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: "anis", userId: 101 };
+users.push(user1);
---
+++ user-defined-type/custom-type.ts
@@ -0,0 +1,6 @@
+let users : object[];
+users = [];
+
+let user1 : { userName : string; userId: number};
+user1 = {userName: "anis", userId:101};
+users.push(user1);
