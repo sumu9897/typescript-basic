68c315d16c77b736457eb505a4cd28a81bcf03d7
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 10:05:51 +0600

    object type

--- user-defined-type/object-type.js
+++ user-defined-type/object-type.js
@@ -1,3 +1,4 @@
+var users;
 var user1;
 user1 = { userName: "mohammad", userId: 101 };
 console.log(user1);
--- user-defined-type/object-type.ts
+++ user-defined-type/object-type.ts
@@ -1,3 +1,8 @@
+let users: object[];
+
+
+
+
 let user1: {userName:string, userId :number}; 
 user1 = {userName: "mohammad", userId :101};
 console.log(user1);
